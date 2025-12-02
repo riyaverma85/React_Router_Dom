bcea05992edcfc35c214be30b593c2914f47e431
riyaverma85 <riyaverma2976@gmail.com>
2025-12-02 15:53:50 -0800

    .

--- riya/src/Hook.jsx
+++ riya/src/Hook.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+
 const App = () => {
   let [count,setcount]=useState(0)
   let Inc=()=>{
@@ -13,7 +14,7 @@ let Dec=()=>{
     <>
     <h1>Counter</h1>
     <p>count:{count}</p>
-    <button onClick={Inc}>Incrimet</button>
+    <button onClick={Inc} >Incrimet</button>
     <button onClick={Dec}>Decriment</button>
     </>
   )
--- riya/src/tailwind.jsx
+++ riya/src/tailwind.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./taiwind.css"
+// import "./taiwind.css"
 import img from "/src/one.jpg"
 import { FaHome } from "react-icons/fa";
 import { IoIosContacts } from "react-icons/io";
@@ -9,7 +9,6 @@ const App = () => {
   return (
     <>
     <header className='flex justify-between px-3'>
-    {/* <div  class="bg-[url('/src/bgimg.jpg')] bg-cover bg-center bg-no-repeat h-96 w-full"> */}
     <img src={img} alt="Logo" class="h-12 w-auto rounded-3xl "  />
     <ul className='flex items-center'>
     <li className='text-cyan-700 p-2 flex '><FaHome />Home</li>
