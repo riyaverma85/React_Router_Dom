59e356c4ade39f10152eefbfe04ba551b7909e96
riyaverma85 <riyaverma2976@gmail.com>
2025-12-02 21:17:32 -0800

    incre_decre

--- riya/src/Hook.jsx
+++ riya/src/Hook.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  let [count,setcount] = useState(0)
+  let Inc=()=>{
+    setcount(count+1)
+    console.log(count);
+  }
+let Dec=()=>{
+  setcount(count-1)
+  console.log(count)
+}
   return (
     <>
-    <h1>Hello Hook</h1>
-    
+    <h1>counter</h1>
+    <p>count:{count}</p>
+    <button onClick={Inc}>Increment</button>
+    <button onClick={Dec}>Decrement</button>
     </>
   )
 }
