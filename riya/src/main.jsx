7bd705d4721680f0f0102088cfd7407e6a7fd059
riyaverma85 <riyaverma2976@gmail.com>
2025-12-02 21:00:25 -0800

    hook

--- riya/src/main.jsx
+++ riya/src/main.jsx
@@ -1,14 +1,14 @@
 
 import { createRoot } from 'react-dom/client'
 
-import App from "./Router.jsx"
-import { BrowserRouter } from 'react-router-dom'
+import App from "./Hook.jsx"
+// import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
     
-    <BrowserRouter>
+    // <BrowserRouter>
           <App/>
-    </BrowserRouter>
+    // </BrowserRouter>
     
   
 )
