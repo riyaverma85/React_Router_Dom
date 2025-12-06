cbfe2c07f9bf44c379ec993c6a4cec08f280bf60
riyaverma85 <riyaverma2976@gmail.com>
2025-12-05 22:51:34 -0800

    form

--- riya/src/Form.jsx
+++ riya/src/Form.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
-const Form = () => {
-    let [name,setname]=useState("")
-    let [email,setemail]=useState("")
-    let Hundlesubmit=(e)=>{
-       e.preventDefault()  /////////////iska use isliye kte hai ki form submit hone pr page reload na ho
-       console.log(name,email);
-    }
-    return (
-    <>
-    <h1>Form handling</h1>
-    <form onSubmit={Hundlesubmit}>
-       Enter your Name<input type="text" value={name} onChange={(e)=>setname(e.target.value)} /><br/>
-       Enter your Email <input type='Email' value={email} onChange={(e)=>setemail(e.target.value)} /><br/>
-        <button type='submit'>save data</button>
-    </form>
-    </>
-  )
-}
-export default Form
+// import React, { useState } from 'react'
+// const Form = () => {
+//     let [name,setname]=useState("")
+//     let [email,setemail]=useState("")
+//     let Hundlesubmit=(e)=>{
+//        e.preventDefault()  /////////////iska use isliye kte hai ki form submit hone pr page reload na ho
+//        console.log(name,email);
+//     }
+//     return (
+//     <>
+//     <h1>Form handling</h1>
+//     <form onSubmit={Hundlesubmit}>
+//        Enter your Name<input type="text" value={name} onChange={(e)=>setname(e.target.value)} /><br/>
+//        Enter your Email <input type='Email' value={email} onChange={(e)=>setemail(e.target.value)} /><br/>
+//         <button type='submit'>save data</button>
+//     </form>
+//     </>
+//   )
+// }
+// export default Form
+
+///////////////////////////////////////////////
