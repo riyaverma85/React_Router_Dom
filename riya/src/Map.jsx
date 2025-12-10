cf061148611b6324e42dc37d769afcf0ebae3b90
riyaverma85 <riyaverma2976@gmail.com>
2025-12-09 21:14:00 -0800

    map

--- riya/src/Map.jsx
+++ riya/src/Map.jsx
@@ -1,16 +1,22 @@
  import React from 'react'
  
  const Map = () => {
-    let MyData=["riya","soni","kumar","singh"]
+    // let MyData=["riya","soni","kumar","singh"]
+
+    let Data=[
+        {id:1,name:"riya",age:22},
+        {id:2,name:"soni",age:21},
+        {id:3,name:"kumar",age:23},
+    ]
    return (
      <>
      <h1>Map</h1>
-     <ul>
-        {
-      MyData.map((e,i)=>(
-      <li key={i}>{e}</li>)
-       ) }
-     </ul>
+ {Data.map((e)=>(
+     <div key={e.id}>
+              <h1>{e.name}</h1>
+              <p>{e.age}</p>
+     </div>  
+    ))}
      </>
    )
  }
