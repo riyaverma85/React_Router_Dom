30e72886234800d09643d3de11379584926ae94f
riyaverma85 <riyaverma2976@gmail.com>
2025-12-10 09:53:00 -0800

    searchbar

---
+++ riya/src/Searchbar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const Searchbar = () => {
+    let [search,setsearch]=useState("")
+
+    let MyData=["riya","soni","kumar","singh","aman","rahul","rohit","ankit"]
+
+    let FilterData=MyData.filter((e)=>{
+        return e.toLowerCase().includes(search.toLowerCase())
+    })
+  return (
+    <>
+    <h1>Map to use searching</h1>
+    Search<input type='text'value={search} onChange={(e)=>{setsearch(e.target.value)}}/>
+    <ul>
+        {FilterData.map((e,i)=>(<li key={i}>{e}</li>))}
+    </ul>
+    </>
+  )
+}
+
+export default Searchbar
