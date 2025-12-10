ccc6995cdbfd4522e0a22bfc1481d2c794244cc7
riyaverma85 <riyaverma2976@gmail.com>
2025-12-09 16:09:23 -0800

    Map

--- riya/src/Map.jsx
+++ riya/src/Map.jsx
@@ -1,1 +1,18 @@
+ import React from 'react'
  
+ const Map = () => {
+    let MyData=["riya","soni","kumar","singh"]
+   return (
+     <>
+     <h1>Map</h1>
+     <ul>
+        {
+MyData.map((e,i)=>(
+<li key={i}>{e}</li>)
+       ) }
+     </ul>
+     </>
+   )
+ }
+ 
+ export default Map
