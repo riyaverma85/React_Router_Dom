c3c1f74f218e8ecf61a248df81ccc41cd483baa9
riyaverma85 <riyaverma2976@gmail.com>
2025-12-11 23:24:51 -0800

    ....

--- riya/src/Map.jsx
+++ riya/src/Map.jsx
@@ -58,7 +58,9 @@ const Map=()=>{
 
   let Data=[
     { Name:"samosa" ,prize:12 ,location:"parihar"},
-    {name:"kachori",prize:14, location:"millan M.P nager"}
+    {name:"kachori",prize:14, location:"millan M.P nager"},
+    {name:"panipuri", prize:10,location:"manpdi"},
+    
   ]
   return(
     <>
