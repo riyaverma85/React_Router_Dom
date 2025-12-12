8e5a1130e3cd52d31510be00cd3d9af010d26700
riyaverma85 <riyaverma2976@gmail.com>
2025-12-11 22:21:44 -0800

    map

--- riya/src/Map.jsx
+++ riya/src/Map.jsx
@@ -1,21 +1,51 @@
- import React from 'react'
- const Map = () => {
-    // let MyData=["riya","soni","kumar","singh"]
-    let Data=[
-        {id:1,name:"Riya verma",age:22},
-        {id:2,name:" Akanchha soni",age:21},
-        {id:3,name:"Rohit kumar",age:23},
-    ]
-   return (
-     <>
-     <h1>Map method</h1>
- {Data.map((r)=>(
-     <div key={r.id}>
-          <h1>{r.name}</h1>
-           <p>{r.age}</p>
-     </div>  
-    ))}
-     </>
-   )
- }
- export default Map
+//  import React from 'react'
+//  const Map = () => {
+//     // let MyData=["riya","soni","kumar","singh"]
+//     let Data=[
+//         {id:1,name:"Riya verma",age:22},
+//         {id:2,name:" Akanchha soni",age:21},
+//         {id:3,name:"Rohit kumar",age:23},
+//     ]
+//    return (
+//      <>
+//      <h1>Map method</h1>
+//  {Data.map((r)=>(
+//      <div key={r.id}>
+//           <h1>{r.name}</h1>
+//            <p>{r.age}</p>
+//      </div>  
+//     ))}
+//      </>
+//    )
+//  }
+//  export default Map
+
+
+
+
+
+
+
+
+
+
+
+
+const Map=()=>{
+  let Data=[
+    {id:1,name:"Riya Verma",age:18}
+  ]
+  return(
+    <>
+    { Data.map((i)=>(
+      <div key={i.id}>
+    <h1>{i.name}</h1>
+    <p>{i.age}</p>
+    </div>
+    )
+  )
+}
+    </>
+  )
+}
+export default Map
