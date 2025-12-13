a0e868368c6efa87b100ff70d38f273716a77e42
riyaverma85 <riyaverma2976@gmail.com>
2025-12-12 23:23:48 -0800

    axios

--- riya/src/DataShow.jsx
+++ riya/src/DataShow.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axios from 'axios'
 const DataShow = () => {
+  useEffect(()=>{
+    let api="http://localhost:3000/Hotel"
+    axios.get(api).then((res)=>{
+      console.log(res.data);
+    }).catch((err)=>{
+      console.log("Error",err);
+    })
+  })
   return (
     <>
-    
+    <h1>heloo</h1>
     
     </>
   )
