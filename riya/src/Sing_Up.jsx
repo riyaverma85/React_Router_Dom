a2ceba950d4c1e137d5c7350bcd24eecd464e7aa
riyaverma85 <riyaverma2976@gmail.com>
2025-12-15 21:21:19 -0800

    .....

--- riya/src/Sing_Up.jsx
+++ riya/src/Sing_Up.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Sing_Up = () => {
   return (
     <>
-    
+    <form>
+Enter your Name<input/>
+Enter your Email<input/>
+Enter your Password<input/>
+    </form>
     
     </>
   )
