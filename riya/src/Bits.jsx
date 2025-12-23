49f22170aaad792708dcc05bb5c429964407977c
riyaverma85 <riyaverma2976@gmail.com>
2025-12-22 22:33:46 -0800

    bit

--- riya/src/Bits.jsx
+++ riya/src/Bits.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import Ballpit from './Ballpit'
-// import "./tailwind.css"
-// const App = () => {
-//   return (
-//     <>
-//  <div style={{position: 'relative', overflow: 'hidden', minHeight: '500px', maxHeight: '500px', width: '100%'}}>
-//   <Ballpit
-//     count={200}
-//     gravity={0.7}
-//     friction={0.8}
-//     wallBounce={0.95}
-//     followCursor={true}
-//   />
-// </div>
-// </>
-//   )
-// }
-
-// export default App
 import React from 'react'
 import Ballpit from './Ballpit'
 import "./tailwind.css"
