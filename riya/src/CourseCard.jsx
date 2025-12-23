27bfea8ebee301386d270847e0be3a5db2f4cda2
riyaverma85 <riyaverma2976@gmail.com>
2025-12-22 22:37:41 -0800

    course

--- riya/src/CourseCard.jsx
+++ riya/src/CourseCard.jsx
@@ -1,12 +1,3 @@
-// function CourseCard({ name, duration }) {
-//   return (
-//      <>
-//     <h2>{name}</h2>
-//       <p>Duration: {duration}</p>
-//       </>
-//   );
-// }
-// export default CourseCard
 function CourseCard({ name, duration }) {
   return (
      <>
