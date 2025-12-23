203bca574c900ca2805664d762022c41c9e22a99
riyaverma85 <riyaverma2976@gmail.com>
2025-12-22 22:34:26 -0800

    child

--- riya/src/Child.jsx
+++ riya/src/Child.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// // import img from "..//src/download.jpg"
-// const Child= ({myname,myage,myprice,myproductname,myimg}) => {
-//   // let[name,age,city]=mydata
-
-//   // let props={
-//   //   myname:"riya",
-//   //   myage:18
-//   // }
-//   // let {myname,myage}=props
-
-//   return (
-//     <div>
-//      <h1>Hello Child </h1>
-//      <img src={img} alt="" />
-//      <h2>My Name is = {myname}</h2>
-//      <h2>My Age is = {myage}</h2>
-//      <h2>My Price is ={myprice}</h2>
-//      <h2>My Product is ={myproductname}</h2>
-//       </div>
-//   )
-// }
-
-// export default Child;
 import React from 'react'
 // import img from "..//src/download.jpg"
 const Child= ({myname,myage,myprice,myproductname,myimg}) => {
