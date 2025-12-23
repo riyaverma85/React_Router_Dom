187e4e10a056007fd52030714ccb8c82fd0335ab
riyaverma85 <riyaverma2976@gmail.com>
2025-12-22 22:32:47 -0800

    ballpit

--- riya/src/main.jsx
+++ riya/src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 
-import App from "./DataShow.jsx"
+import App from "./Bits.jsx"
 // import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
