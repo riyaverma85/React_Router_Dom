9867a3ad20e76ef0e0142a726992106b7e4c3b51
riyaverma85 <riyaverma2976@gmail.com>
2026-01-04 22:08:53 -0800

    .,.,

--- riya/src/Layout.jsx
+++ riya/src/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet,Link } from 'react-router-dom'
 const Layout = () => {
   return (
     <>
-    <header>
+     <header>
         <ul>
             <li> <Link to="/">Home</Link></li>
             <li><Link to="about">About</Link></li>
