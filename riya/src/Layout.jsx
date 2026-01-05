1840ca7de1c3fca7c5d5ccbf683203d5c81d168a
riyaverma85 <riyaverma2976@gmail.com>
2026-01-04 22:10:53 -0800

    ,.,

--- riya/src/Layout.jsx
+++ riya/src/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
         <ul>
             <li> <Link to="/">Home</Link></li>
             <li><Link to="about">About</Link></li>
-            <li><Link to="service">Service</Link></li>
+             <li><Link to="service">Service</Link></li>
              <li><Link to="contect">Contect</Link></li>
         </ul>
      </header>
