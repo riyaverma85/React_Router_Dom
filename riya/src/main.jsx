a819c6852f202c9decdac2f5989993b3a622ce77
riyaverma85 <riyaverma2976@gmail.com>
2026-01-07 17:42:47 -0800

    .,.,

--- riya/src/main.jsx
+++ riya/src/main.jsx
@@ -1,7 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 
-import App from "./App.jsx"
+import App from "./Student.jsx"
 // import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document . getElementById ('root')) . render(
