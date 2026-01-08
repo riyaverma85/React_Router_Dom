639a367e659f8c531ad8f9be3d72448e4d0d0ad1
riyaverma85 <riyaverma2976@gmail.com>
2026-01-07 17:49:37 -0800

    .,.,

--- riya/src/props.jsx
+++ riya/src/props.jsx
@@ -5,7 +5,8 @@ const App = () => {
     <>
     <h1>home page</h1>
          {/* <Child myname={name} myage={age} myprice={price} myproductname={productname}/> */}
-         <Child myname="riya" myage={18} myprice={400} myproductname="one plush nord"/>
+         {/* <Child myname="riya" myage={18} myprice={400} myproductname="one plush nord"/> */}
+         <Child myname="Riya" course="React"/>
     </>
   )
 }
