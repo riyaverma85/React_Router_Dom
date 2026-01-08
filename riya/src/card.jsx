2c0f2b55ec580983036e8d7ca47958178a696a3c
riyaverma85 <riyaverma2976@gmail.com>
2026-01-07 18:36:37 -0800

    .,.,

--- riya/src/card.jsx
+++ riya/src/card.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./tailwind.css"
+import { Color } from 'three/src/Three.Core.js'
 const card = () => {
   return (
-    <div className='bg-blue-600' 
-    >card</div>
+    <div className='bg-blue-600 p-30 m-6 mr-100 ml-100 rounded-2xl shadow-2xl ' 
+    > <p className="text-white hover:text-emerald-300 hover:scale-110 transition duration-300 ease-in-out">
+    card
+  </p>
+</div>
   )
 }
 
