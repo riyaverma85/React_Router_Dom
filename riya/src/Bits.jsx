fd2bcf3d3ef49510f6ca0fe8320f4fc0ce347f6a
riyaverma85 <riyaverma2976@gmail.com>
2026-01-09 22:32:19 -0800

    ..,.,

--- riya/src/Bits.jsx
+++ riya/src/Bits.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
   <div style={{position: 'relative', overflow: 'hidden', minHeight: '500px', maxHeight: '500px', width: '100%'}}>
-  <Ballpit
+   <Ballpit
     count={200}
     gravity={0.7}
     friction={0.8}
