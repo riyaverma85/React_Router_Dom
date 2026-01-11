10f3ce654bb0cefb7960c756b7d04a13f7d6ad5e
riyaverma85 <riyaverma2976@gmail.com>
2026-01-10 23:21:13 -0800

    .,.,

--- riya/src/Bits.jsx
+++ riya/src/Bits.jsx
@@ -7,7 +7,7 @@ const App = () => {
   <div  style={{position: 'relative', overflow: 'hidden', minHeight: '500px', maxHeight: '500px', width: '100%'}}>
    <Ballpit
      count={200}
-    gravity={0.7}
+     gravity={0.7}
     friction={0.8}
     wallBounce={0.95}
     followCursor={true}
