97a17253d674e948302e922f017fb77f17231314
riyaverma85 <riyaverma2976@gmail.com>
2026-01-11 21:12:11 -0800

    .,.

--- riya/src/Form.jsx
+++ riya/src/Form.jsx
@@ -127,7 +127,7 @@ const App = () => {
       
       Select City:
        <select name='city' value={form.city} onChange={Handlechange}>
-          <option value= "Bhopal">Bhopal</option>
+          <option value= "Bhopal" >Bhopal</option>
           <option value= "sehore" >Sehore</option>
           <option value= "indore">Indore</option>
           <option value= "delhi">Delhi</option>
