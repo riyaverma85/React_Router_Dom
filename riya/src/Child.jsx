2bb789eeaad568c741a041baeaf3275491fd34ce
riyaverma85 <riyaverma2976@gmail.com>
2026-01-22 20:20:35 -0800

    .,,.,.

--- riya/src/Child.jsx
+++ riya/src/Child.jsx
@@ -29,7 +29,7 @@ import React from 'react'
 const Child = ({myname,course}) => {
   return (
        <div>
-           <h1> Hello,my name is {myname} </h1>
+            <h1> Hello,my name is {myname} </h1>
            <h1> and i am lurning {course} </h1>
        </div>
   )
