dd7e50f793549e4374878730e168bba8c6234669
riyaverma85 <riyaverma2976@gmail.com>
2026-01-30 22:25:59 -0800

    .,

--- riya/src/Mybooking.jsx
+++ riya/src/Mybooking.jsx
@@ -39,7 +39,7 @@ const App = () => {
     <tbody>
         {Data.map((e)=>(
         <tr style={{ border: "2px solid black" }} key={e.id}>
-            <td>{e.moviename}</td>
+             <td>{e.moviename}</td>
             <td>{e.date}</td>
             <td>{e.city}</td>
             <td>{e.person}</td>
