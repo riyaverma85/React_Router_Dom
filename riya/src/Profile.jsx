75205f544e03f5e7c31b2f4362f9cc96fd373c3c
riyaverma85 <riyaverma2976@gmail.com>
2026-02-13 22:41:43 -0800

    .,

--- riya/src/Profile.jsx
+++ riya/src/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = ({  img,name, city }) => {
   return (
     <>
         <h1>{img}</h1>
-       <h1>Name is = {name}</h1>
+        <h1>Name is = {name}</h1>
        <h1>City is = {city}</h1>
     </>
   )
