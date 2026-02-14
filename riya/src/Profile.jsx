ca23a558a0a8fd7b6a50f1914a660e527297c3cc
riyaverma85 <riyaverma2976@gmail.com>
2026-02-13 22:39:35 -0800

    ,.,.

--- riya/src/Profile.jsx
+++ riya/src/Profile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Profile = ({  img,name, city }) => {
   return (
     <>
-      <h1>{img}</h1>
+       <h1>{img}</h1>
       <h1>Name is = {name}</h1>
       <h1>City is = {city}</h1>
     </>
